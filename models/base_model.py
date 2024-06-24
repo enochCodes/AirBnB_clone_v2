7eb75e1f656bd94c928098e9822d1ba60ccc3216
Badre Ziani <badreziani@gmail.com>
2024-06-24 15:30:34 +0100

    Update on base_model

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,9 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow)
+    # id = Column(String(60), nullable=False, primary_key=True)
+    # created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
