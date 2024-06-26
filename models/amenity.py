edf14677bf41cd650909905efdd68047433e3ccb
Badre Ziani <badreziani@gmail.com>
2024-06-26 15:32:19 +0100

    Task 10 first steps

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlachemy import Table, Column, String
+
+from models.base_model import BaseModel, Base
+import models
 
 
-class Amenity(BaseModel):
-    name = ""
+association_table = Table('place_amenities', Base.metadata)
+
+
+class Amenity(BaseModel, Base):
+    if models.storage_type == 'db':
+        __tablename__ = 'amenities'
+
+        name = Column(String(128), nullable=False)
+        place_amenities = 
+    else:
+        name = ""
