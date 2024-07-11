a9e5144107c504dcad18ee09a39f4b3f7ab71f8b
Badre Ziani <badreziani@gmail.com>
2024-07-11 15:17:08 +0100

    Fix bugs on task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,23 +4,23 @@
 This script generates a .tgz archive from the contents of the web_static
 folder of the AirBnB Clone repo, using the function do_pack
 """
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
-
-env.user = "ubuntu"
-env.hosts = ["localhost"]
 
 def do_pack():
     """Generates .tgz file
     from the content of the web_static"""
-    now = datetime.now()
+    now = datetime.utcnow()
     year = now.year
     month = now.month if now.month > 9 else f"0{now.month}"
     day = now.day if now.day > 9 else f"0{now.day}"
     hour = now.hour if now.hour > 9 else f"0{now.hour}"
     minute = now.minute if now.minute > 9 else f"0{now.minute}"
     second = now.second if now.second > 9 else f"0{now.second}"
-    arch_name = f"web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
+    path = f"versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
     recource = "/data/web_static/"
-    run("mkdir -p versions")
-    run(f"tar -czvf versions/{arch_name} {resource}")
+    if local("mkdir -p versions").failed:
+        return None
+    if local(f"tar -czvf {path} {resource}").failed:
+        return None
+    return path
